6784aa01f95704060859a68530cee488debbb1e7
焦 <983487889@qq.com>
2020-02-24 19:30:59 +0800

    first init jcart stroe back

---
+++ jart-store-back/src/main/java/io/jza/jartstoreback/JartStoreBackApplication.java
@@ -0,0 +1,13 @@
+package io.jza.jartstoreback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JartStoreBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JartStoreBackApplication.class, args);
+    }
+
+}
---
+++ jart-store-back/src/test/java/io/jza/jartstoreback/JartStoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jza.jartstoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JartStoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
