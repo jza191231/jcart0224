7c452911c20cece2466a16a522ad2751af4f84cd
焦 <983487889@qq.com>
2020-02-24 19:25:46 +0800

    first init jcart administrat

---
+++ jart-administration-back/src/main/java/io/jza/jartadministrationback/JartAdministrationBackApplication.java
@@ -0,0 +1,13 @@
+package io.jza.jartadministrationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JartAdministrationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JartAdministrationBackApplication.class, args);
+    }
+
+}
---
+++ jart-administration-back/src/test/java/io/jza/jartadministrationback/JartAdministrationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jza.jartadministrationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JartAdministrationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
