ded2f64e453a3248ad5fb60b47258c30420a03fa
焦 <983487889@qq.com>
2020-02-26 17:02:58 +0800

    add Product All

--- jart-administration-back/src/main/java/io/jza/jcartadministrationback/controller/ProductController.java
+++ jart-administration-back/src/main/java/io/jza/jcartadministrationback/controller/ProductController.java
@@ -1,6 +1,7 @@
 package io.jza.jcartadministrationback.controller;
 
 
+import com.github.pagehelper.Page;
 import io.jza.jcartadministrationback.dto.in.ProductCreateInDTO;
 import io.jza.jcartadministrationback.dto.in.ProductSearchInDTO;
 import io.jza.jcartadministrationback.dto.in.ProductUpdateInDTO;
@@ -11,6 +12,8 @@ import io.jza.jcartadministrationback.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
@@ -20,13 +23,23 @@ public class ProductController {
 
     @GetMapping("/search")
     public PageOutDTO<ProductListOutDTO> search(ProductSearchInDTO productSearchInDTO,
-                                                @RequestParam Integer pageNum){
-        return null;
+                                                @RequestParam(required = false,defaultValue = "1") Integer pageNum){
+        Page<ProductListOutDTO> page = productService.search(pageNum);
+        PageOutDTO<ProductListOutDTO> pageOutDTO = new PageOutDTO<>();
+        pageOutDTO.setTotal(page.getTotal());
+        pageOutDTO.setPageSize(page.getPageSize());
+        pageOutDTO.setPageNum(page.getPageNum());
+        pageOutDTO.setList(page);
+
+        return pageOutDTO;
+
     }
 
     @GetMapping("/getById")
     public ProductShowOutDTO getById(@RequestParam Integer productId){
-        return null;
+        ProductShowOutDTO productShowOutDTO = productService.getById(productId);
+        return productShowOutDTO;
+
     }
 
     @PostMapping("/create")
@@ -36,7 +49,16 @@ public class ProductController {
 
     @PostMapping("/update")
     public void update(@RequestBody ProductUpdateInDTO productUpdateInDTO){
+        productService.update(productUpdateInDTO);
+    }
+    @PostMapping("/delete")
+    public void delete(@RequestBody Integer productId){
+        productService.delete(productId);
+    }
 
+    @PostMapping("/batchDelete")
+    public void batchDelete(@RequestBody List<Integer> productIds){
+        productService.batchDelete(productIds);
     }
 
 }
--- jart-administration-back/src/main/java/io/jza/jcartadministrationback/service/impl/ProductServiceImpl.java
+++ jart-administration-back/src/main/java/io/jza/jcartadministrationback/service/impl/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package io.jza.jcartadministrationback.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import io.jza.jcartadministrationback.dao.ProductDetailMapper;
 import io.jza.jcartadministrationback.dao.ProductMapper;
 import io.jza.jcartadministrationback.dto.in.ProductCreateInDTO;
@@ -54,27 +55,73 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @Transactional
     public void update(ProductUpdateInDTO productUpdateInDTO) {
+         Product product = new Product();
+        product.setProductId(productUpdateInDTO.getProductId());
+        product.setProductName(productUpdateInDTO.getProductName());
+        product.setPrice(productUpdateInDTO.getPrice());
+        product.setDiscount(productUpdateInDTO.getDiscount());
+        product.setStockQuantity(productUpdateInDTO.getStockQuantity());
+        product.setMainPicUrl(productUpdateInDTO.getMainPicUrl());
+        product.setStatus(productUpdateInDTO.getStatus());
+        product.setRewordPoints(productUpdateInDTO.getRewordPoints());
+        product.setSortOrder(productUpdateInDTO.getSortOrder());
+        String description = productUpdateInDTO.getDescription();
+        String productAbstract = description.substring(0, Math.min(100, description.length()));
+        product.setProductAbstract(productAbstract);
+        productMapper.updateByPrimaryKeySelective(product);
 
+        ProductDetail productDetail = new ProductDetail();
+        productDetail.setProductId(productUpdateInDTO.getProductId());
+        productDetail.setDescription(productUpdateInDTO.getDescription());
+        List<String> otherPicUrls = productUpdateInDTO.getOtherPicUrls();
+        productDetail.setOtherPicUrls(JSON.toJSONString(otherPicUrls));
+        productDetailMapper.updateByPrimaryKeySelective(productDetail);
     }
 
     @Override
+    @Transactional
     public void delete(Integer productId) {
-
+        productMapper.deleteByPrimaryKey(productId);
+        productDetailMapper.deleteByPrimaryKey(productId);
     }
 
     @Override
+    @Transactional
     public void batchDelete(List<Integer> productIds) {
-
+         productMapper.batchDelete(productIds);
+         productDetailMapper.batchDelete(productIds);
     }
 
     @Override
     public Page<ProductListOutDTO> search(Integer pageNum) {
-        return null;
+        PageHelper.startPage(pageNum,10);
+        Page<ProductListOutDTO> page = productMapper.search();
+        return page;
     }
 
     @Override
     public ProductShowOutDTO getById(Integer productId) {
-        return null;
+        Product product = productMapper.selectByPrimaryKey(productId);
+        ProductDetail productDetail = productDetailMapper.selectByPrimaryKey(productId);
+
+        ProductShowOutDTO productShowOutDTO = new ProductShowOutDTO();
+        productShowOutDTO.setProductId(productId);
+        productShowOutDTO.setProductCode(product.getProductCode());
+        productShowOutDTO.setProductName(product.getProductName());
+        productShowOutDTO.setPrice(product.getPrice());
+        productShowOutDTO.setDiscount(product.getDiscount());
+        productShowOutDTO.setStatus(product.getStatus());
+        productShowOutDTO.setMainPicUrl(product.getMainPicUrl());
+        productShowOutDTO.setRewordPoints(product.getRewordPoints());
+        productShowOutDTO.setSortOrder(product.getSortOrder());
+        productShowOutDTO.setStockQuantity(product.getStockQuantity());
+
+        productShowOutDTO.setDescription(productDetail.getDescription());
+        String otherPicUrlsJson = productDetail.getOtherPicUrls();
+        List<String> otherPicUrls = JSON.parseArray(otherPicUrlsJson, String.class);
+        productShowOutDTO.setOtherPicUrls(otherPicUrls);
+        return productShowOutDTO;
     }
 }
