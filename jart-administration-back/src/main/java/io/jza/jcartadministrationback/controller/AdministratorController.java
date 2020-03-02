db6b3ed68b070ccba485934422f3aa01bfbd1389
焦 <983487889@qq.com>
2020-03-02 19:12:24 +0800

    后台登录

--- jart-administration-back/src/main/java/io/jza/jcartadministrationback/controller/AdministratorController.java
+++ jart-administration-back/src/main/java/io/jza/jcartadministrationback/controller/AdministratorController.java
@@ -1,10 +1,14 @@
 package io.jza.jcartadministrationback.controller;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import io.jza.jcartadministrationback.constant.ClientExceptionConstant;
 import io.jza.jcartadministrationback.dto.in.*;
-import io.jza.jcartadministrationback.dto.out.AdministratorGetProfileOutDTO;
-import io.jza.jcartadministrationback.dto.out.AdministratorListOutDTO;
-import io.jza.jcartadministrationback.dto.out.AdministratorShowOutDTO;
-import io.jza.jcartadministrationback.dto.out.PageOutDTO;
+import io.jza.jcartadministrationback.dto.out.*;
+import io.jza.jcartadministrationback.exception.ClientException;
+import io.jza.jcartadministrationback.po.Administrator;
+import io.jza.jcartadministrationback.service.AdministratorService;
+import io.jza.jcartadministrationback.util.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,9 +17,27 @@ import java.util.List;
 @RequestMapping("/administrator")
 public class AdministratorController {
 
+    @Autowired
+    private AdministratorService administratorService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
     @GetMapping("/login")
-    public String login(AdministratorLogininInDTO administratorLogininInDTO){
-         return null;
+    public AdministratorLoginOutDTO login(AdministratorLogininInDTO administratorLoginInDTO) throws ClientException {
+        Administrator administrator = administratorService.getByUsername(administratorLoginInDTO.getUsername());
+        if (administrator == null){
+            throw new ClientException(ClientExceptionConstant.ADMINISTRATOR_USERNAME_NOT_EXIST_ERRCODE, ClientExceptionConstant.ADMINISTRATOR_USERNAME_NOT_EXIST_ERRMSG);
+        }
+        String encPwdDB = administrator.getEncryptedPassword();
+        BCrypt.Result result = BCrypt.verifyer().verify(administratorLoginInDTO.getPassword().toCharArray(), encPwdDB);
+
+        if (result.verified) {
+            AdministratorLoginOutDTO administratorLoginOutDTO = jwtUtil.issueToken(administrator);
+            return administratorLoginOutDTO;
+        }else {
+            throw new ClientException(ClientExceptionConstant.ADNINISTRATOR_PASSWORD_INVALID_ERRCODE, ClientExceptionConstant.ADNINISTRATOR_PASSWORD_INVALID_ERRMSG);
+        }
     }
 
     @GetMapping("/getProfile")
