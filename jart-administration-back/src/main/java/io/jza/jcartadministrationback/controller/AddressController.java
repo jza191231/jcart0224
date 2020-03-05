67045c7b472e893dcc64777363c20531d51a686d
焦 <983487889@qq.com>
2020-03-05 19:59:53 +0800

    add administration address get list by customerId api

--- jart-administration-back/src/main/java/io/jza/jcartadministrationback/controller/AddressController.java
+++ jart-administration-back/src/main/java/io/jza/jcartadministrationback/controller/AddressController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/address")
@@ -21,7 +22,17 @@ public class AddressController {
 
     @GetMapping("/getListByCustomerId")
     public List<AddressListOutDTO> getListByCustomerId(@RequestParam Integer customerId){
-        return null;
+        List<Address> addresses = addressService.getByCustomerId(customerId);
+        List<AddressListOutDTO> AddressListOutDTOS = addresses.stream().map(address -> {
+            AddressListOutDTO addressListOutDTO = new AddressListOutDTO();
+            addressListOutDTO.setAddressId(address.getAddressId());
+            addressListOutDTO.setReceiverName(address.getReceiverName());
+            addressListOutDTO.setReceiverMobile(address.getReceiverMobile());
+            addressListOutDTO.setTag(address.getTag());
+            addressListOutDTO.setContent(address.getContent());
+            return addressListOutDTO;
+        }).collect(Collectors.toList());
+        return AddressListOutDTOS;
     }
 
     @GetMapping("/getById")
--- jart-administration-back/src/main/java/io/jza/jcartadministrationback/service/AddressService.java
+++ jart-administration-back/src/main/java/io/jza/jcartadministrationback/service/AddressService.java
@@ -2,7 +2,11 @@ package io.jza.jcartadministrationback.service;
 
 import io.jza.jcartadministrationback.po.Address;
 
+import java.util.List;
+
 public interface AddressService {
 
     Address getById(Integer addressId);
+
+    List<Address> getByCustomerId(Integer customerId);
 }
