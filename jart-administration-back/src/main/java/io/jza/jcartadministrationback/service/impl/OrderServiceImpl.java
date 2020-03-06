5fe41bd6f0bb8d2bec358080e1dc450a36c9cd12
焦 <983487889@qq.com>
2020-03-06 09:19:39 +0800

    add administration order show api

--- jart-administration-back/src/main/java/io/jza/jcartadministrationback/service/impl/OrderServiceImpl.java
+++ jart-administration-back/src/main/java/io/jza/jcartadministrationback/service/impl/OrderServiceImpl.java
@@ -1,16 +1,22 @@
 package io.jza.jcartadministrationback.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import io.jza.jcartadministrationback.dao.OrderDetailMapper;
 import io.jza.jcartadministrationback.dao.OrderMapper;
 import io.jza.jcartadministrationback.dto.out.OrderListOutDTO;
 import io.jza.jcartadministrationback.dto.out.OrderShowOutDTO;
+import io.jza.jcartadministrationback.po.Customer;
 import io.jza.jcartadministrationback.po.Order;
 import io.jza.jcartadministrationback.po.OrderDetail;
+import io.jza.jcartadministrationback.service.CustomerService;
 import io.jza.jcartadministrationback.service.OrderService;
+import io.jza.jcartadministrationback.vo.OrderProductVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -19,6 +25,8 @@ public class OrderServiceImpl implements OrderService {
     private OrderMapper orderMapper;
     @Autowired
     private OrderDetailMapper orderDetailMapper;
+    @Autowired
+    private CustomerService customerService;
 
     @Override
     public Page<OrderListOutDTO> search(Integer pageNum) {
@@ -31,8 +39,12 @@ public class OrderServiceImpl implements OrderService {
     public OrderShowOutDTO getById(Long orderId) {
         Order order = orderMapper.selectByPrimaryKey(orderId);
         OrderDetail orderDetail = orderDetailMapper.selectByPrimaryKey(orderId);
+
         OrderShowOutDTO orderShowOutDTO = new OrderShowOutDTO();
         orderShowOutDTO.setOrderId(orderId);
+        orderShowOutDTO.setCustomerId(order.getCustomerId());
+        Customer customer = customerService.getById(order.getCustomerId());
+        orderShowOutDTO.setCustomerName(customer.getRealName());
         orderShowOutDTO.setStatus(order.getStatus());
         orderShowOutDTO.setTotalPrice(order.getTotalPrice());
         orderShowOutDTO.setRewordPoints(order.getRewordPoints());
@@ -46,7 +58,11 @@ public class OrderServiceImpl implements OrderService {
         orderShowOutDTO.setInvoiceAddress(orderDetail.getInvoiceAddress());
         orderShowOutDTO.setInvoicePrice(orderDetail.getInvoicePrice());
         orderShowOutDTO.setComment(orderDetail.getComment());
-        return null;
+
+        List<OrderProductVO> orderProductVOS = JSON.parseArray(orderDetail.getOrderProducts(), OrderProductVO.class);
+        orderShowOutDTO.setOrderProducts(orderProductVOS);
+
+        return orderShowOutDTO;
     }
 
 }
